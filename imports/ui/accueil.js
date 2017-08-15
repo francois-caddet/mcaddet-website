b32c4611122fa4198b493dd087cd73078a822e3a
Etienne CADDET <etiennecaddet@MacBook-Air-de-Etienne.local>
2017-08-15 18:00:23 +0200

    remise au propre du accueil.js

--- imports/ui/accueil.js
+++ imports/ui/accueil.js
@@ -1,8 +1,5 @@
 top();
 
-function hasSameColor(color, can) {
-  return can.color === color;
-}
 
 function top() {
 	var can = document.getElementById('top');
@@ -16,21 +13,6 @@ function top() {
 	ctx.fillStyle='red';
 	ctx.fill();
 	
-	can.addEventListener('click', (e) => {
-	  const mousePos = {
-	    x: e.clientX - can.offsetTop,
-	    y: e.clientY - can.offsetLeft
-	  };
-	  // get pixel under cursor
-	  const pixel = ctx.getImageData(mousePos.x, mousePos.y, 1, 1).data;
-
-	  // create rgb color for that pixel
-	  const color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
-
-	  // find a circle with the same colour
-	    if (hasSameColor(color, can)) {
-	      alert('click on circle: ' + can.id);
-	 }
 }
 
 
